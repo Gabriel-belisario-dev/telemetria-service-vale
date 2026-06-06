3ff62703f54ee73a89a77c0d13e4ccd757773586
Gabriel Belisario <gabriel.hb.rocha.97@gmail.com>
2026-06-06 03:54:33 -0300

    feat: expondo a inteligência preditiva via API (FastAPI + Swagger)
    O cérebro do nosso sistema agora fala com o mundo! Integrei o motor analítico diretamente na rota '/analise/top-caminhoes' da nossa API. Agora, qualquer sistema ou dashboard da Vale pode fazer uma requisição e descobrir em tempo real quais equipamentos estão em risco crítico. O teste no Swagger UI foi um sucesso absoluto e os dados já estão fluindo perfeitamente no formato JSON, isolando o ruído e mostrando o Top 5 da frota.

--- app/main.py
+++ app/main.py
@@ -1,16 +1,41 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI(
-    title="Vale Minitruck Telemetry API",
-    description="API de missão crítica para monitoramento de frotas e predição de alarmes.",
+    title="API de Telemetria e Monitoramento - Vale",
+    description="Motor preditivo para análise de frota de caminhões.",
     version="1.0.0"
 )
 
-@app.get("/health", tags=["Infrastructure"])
-async def health_check():
+# Caminhos dos dados locais
+TELEMETRIA_LIMPA = "data/telemetria_limpa.parquet"
+REGRAS = "data/Alarmes - Regra de Negocio.xlsx"
+
+@app.get("/")
+def home():
+    return {"status": "Motor da API ligado e operando com sucesso!"}
+
+@app.get("/analise/top-caminhoes")
+def obter_caminhoes_criticos():
+    """
+    Cruza a base de telemetria limpa com as regras de negócio
+    e retorna o Top 5 de caminhões com mais alertas de perigo.
+    """
+    # Carrega dados
+    df_dados = pd.read_parquet(TELEMETRIA_LIMPA)
+    df_regras = pd.read_excel(REGRAS, engine='openpyxl')
+    
+    # Padroniza as regras
+    df_regras['NIVEL'] = df_regras['NIVEL'].str.upper()
+    eventos_perigosos = df_regras['EVENTO'].unique()
+    
+    # Filtra as ocorrências reais
+    ocorrencias_reais = df_dados[df_dados['Alarme'].isin(eventos_perigosos)]
+    
+    # Conta os top 5 e converte para um formato que a API entende (Dicionário)
+    top_5 = ocorrencias_reais['Tag_Frota'].value_counts().head(5).to_dict()
+    
     return {
-        "status": "healthy",
-        "environment": "development",
-        "version": "1.0.0",
-        "message": "Microsserviço de Telemetria Vale operando perfeitamente."
+        "total_alertas_encontrados": len(ocorrencias_reais),
+        "ranking_caminhoes_criticos": top_5
     }
